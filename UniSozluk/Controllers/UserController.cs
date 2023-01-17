d68c6c70e445c36cf2f4c6001b1f2713396f723f
Gürkan Merter <grknmrtr6116@gmail.com>
2023-01-17 21:22:39 +0300

    User profili düzeltildi, user düzenleme ekranı getirildi üye ol ve kayıt ol navbara eklendi

--- UniSozluk/Controllers/UserController.cs
+++ UniSozluk/Controllers/UserController.cs
@@ -1,8 +1,11 @@
 ﻿using BussinesLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace UniSozluk.Controllers
@@ -11,6 +14,7 @@ namespace UniSozluk.Controllers
     public class UserController : Controller
     {
         UserManager usm = new UserManager(new EfUserRepository());
+        DepartmantManager dm = new DepartmantManager(new EfDepartmantRepository());
         Context context = new Context();
 
         [HttpGet]
@@ -21,5 +25,34 @@ namespace UniSozluk.Controllers
             ViewBag.userEntryCount = context.Entries.Where(x=>x.UserID == id).Count();
             return View(value);
         }
+
+        [HttpGet]
+        public IActionResult UserEdit(int id)
+        {
+            var user = usm.GetUserWithUniversityByID(id);
+
+
+            List<SelectListItem> DepartmantValue = (from x in dm.GetListByUniversityID((int)user.Departmant.UniversityID)
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = x.DepartmantName,
+                                                        Value = x.DepartmantID.ToString()
+                                                    }
+                                                    ).ToList();
+            ViewBag.depValue = DepartmantValue;
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult UserEdit(User user)
+        {
+            usm.TUpdate(user);
+            return RedirectToAction("Index");
+        }
+
+
+
+        
     }
 }
